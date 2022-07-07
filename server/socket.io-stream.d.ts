693962dc4494a4032010cb8c68cd2dbbdadbb374
roerohan <rohan@dyte.in>
2022-07-07 18:43:50 +0530

    fix: use ts-node instead of nodemon

--- server/socket.io-stream.d.ts
+++ server/socket.io-stream.d.ts
@@ -1,3 +1,4 @@
 declare module 'socket.io-stream' {
     export default function ss(client: any): any;
+    export function createStream(): any;
 }
---
+++ server/stream.spec.ts
@@ -0,0 +1,9 @@
+// import { pipeline } from 'stream';
+// import { io } from 'socket.io-client';
+// import ss from 'socket.io-stream';
+
+// const socket = io('http://localhost:3001');
+// socket.on('connect', () => {
+//     const stream = ss.createStream();
+
+// });
